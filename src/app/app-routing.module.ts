ee87c2fd56cb52dd1dd10d579ca961d9f63fbe83
Ignacio <nachogrignola@gmail.com>
2021-11-13 11:56:23 +0100

    se creo el guard y el componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
 
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'login', component:LoginComponent},
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
   {path:'**', redirectTo:'login', pathMatch:'full'}
 
 ];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router:Router, private serviceLogin:LoginService){}
+
+  canActivate(){
+
+    if( this.serviceLogin.estaAutenticado()){
+      return true
+    } else{
+      this.router.navigate(['/login'])
+      return false;
+    } 
+
+  }
+  
+}
