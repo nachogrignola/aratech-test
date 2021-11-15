feac470d24684a70ccc5cc7ac9ddec8a810c688c
nachogrignola <78497456+nachogrignola@users.noreply.github.com>
2021-11-15 14:15:58 +0100

    Revert "Feature home"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { Interceptor } from './services/interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: Interceptor,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/model/usuario.model.ts
+++
@@ -1,3 +0,0 @@
-export class Usuario {
-   constructor(public email: string,public password: string){} 
-}
